21135dc16081d9197033248a05af328b56998c22
Tatiana <tmeneses9712@gmail.com>
2024-11-21 00:24:56 +0000

    exercise corrected

--- Exercises/my_network_tool.sh
+++ Exercises/my_network_tool.sh
@@ -16,25 +16,31 @@ echo "Welcome to My Network Tool!"
 case $option in
     "1")
     echo "ifconfig"
+    ifconfig
     ;;
 
     "2")
     echo "ping 192.168.1.252"
+    ping 192.168.1.252
     ;;
 
     "3")
     echo "nmap 192.168.1.252"
+    nmap 192.168.1.252
     ;;
 
     "4")
     echo "netstat -rn"
+    netstat -rn
     ;;
 
     "5")
     echo "traceroute 192.168.1.252"
+    traceroute 192.168.1.252
     ;;
 
     "6")
     echo "exit"
+    exit
     ;;
 esac
